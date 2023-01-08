114230e5198c47123b3976071d65fa76519fe975
André L. C. Moreira <alcm99@gmail.com>
2023-01-08 16:16:27 -0300

    addin THREAD_SAFE_ZONE macro to delimit the thread safe scope of a given function

--- src/queue.c
+++ src/queue.c
@@ -3,6 +3,11 @@
 
 #include "queue.h"
 
+#define THREAD_SAFE_ZONE(mutex, code)  \
+  pthread_mutex_lock(mutex); \
+  code \
+  pthread_mutex_unlock(mutex);
+
 struct task_queue_t {
   task_t *tail;
   task_t *head;
@@ -12,19 +17,17 @@ struct task_queue_t {
 int queue_size(task_queue_t *queue) {
   int size = 0;
 
-  pthread_mutex_lock(&queue->mutex);
-
-  for_each_task(curr, queue) {
-    size++;
-  }
-
-  pthread_mutex_unlock(&queue->mutex);
+  THREAD_SAFE_ZONE(&queue->mutex, {
+    for_each_task(curr, queue) {
+      size++;
+    }
+  })
 
   return size;
 }
 
 task_t *create_task(void (*task)(void *), void *param) {
-  struct task_t *node = NULL;
+  task_t *node;
 
   node = (task_t *)malloc(sizeof(task_t));
   if (node) {
@@ -37,38 +40,36 @@ task_t *create_task(void (*task)(void *), void *param) {
 }
 
 void enqueue(task_queue_t *queue, task_t *elem) {
-  pthread_mutex_lock(&queue->mutex);
-
-  /* if the queue is empty */
-  if (!queue->head && !queue->tail) {
-    queue->tail = queue->head = elem;
-  } else {
-    queue->tail->prev = elem;
-    elem->next = queue->tail;
-    queue->tail = elem;
-  }
-
-  pthread_mutex_unlock(&queue->mutex);
+  THREAD_SAFE_ZONE(&queue->mutex, {
+    // if the queue is empty
+    if (!queue->head && !queue->tail) {
+      queue->tail = queue->head = elem;
+    } else {
+      queue->tail->prev = elem;
+      elem->next = queue->tail;
+      queue->tail = elem;
+    }
+  })
 }
 
-struct task_t *dequeue(task_queue_t *queue) {
-  struct task_t *to_del = queue->head;
+task_t *dequeue(task_queue_t *queue) {
+  task_t *to_del;
 
-  pthread_mutex_lock(&queue->mutex);
+  THREAD_SAFE_ZONE(&queue->mutex, {
+    to_del = queue->head;
 
-  /* if the list is not empty */
-  if (to_del) {
-    queue->head = to_del->prev;
-    /* if the queue has more than one element */
-    if (to_del->prev) {
-      to_del->prev->next = NULL;
-      to_del->prev = NULL;
-    } else {
-      queue->head = queue->tail = NULL;
+    // if the list is not empty
+    if (to_del) {
+      queue->head = to_del->prev;
+      // if the queue has more than one element
+      if (to_del->prev) {
+        to_del->prev->next = NULL;
+        to_del->prev = NULL;
+      } else {
+        queue->head = queue->tail = NULL;
+      }
     }
-  }
-
-  pthread_mutex_unlock(&queue->mutex);
+  })
 
   return to_del;
 }
@@ -84,7 +85,7 @@ void init_queue(task_queue_t **queue) {
 
 void destroy_queue(task_queue_t *queue) {
   for_each_task_safe(curr, queue) {
-    struct task_t *node = dequeue(queue);
+    task_t *node = dequeue(queue);
 
     free(node);
   }
