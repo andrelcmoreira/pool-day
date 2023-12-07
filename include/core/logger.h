e8214c8c47089fe6d766f1d1efe873cecd1714fd
andré <andrelcmoreira@disroot.org>
2023-12-07 00:51:18 -0300

    simplifying the logger

--- include/core/logger.h
+++ include/core/logger.h
@@ -17,30 +17,21 @@
 
 #ifdef LIB_LOGGING
 #define POOL_DAY_LOG(...) \
-  log_msg(LOG_SEVERITY_INFO, LOG_HEADER " | " LOG_TAG_INFO ": " __VA_ARGS__)
+  log_msg(LOG_HEADER " | " LOG_TAG_INFO ": " __VA_ARGS__)
 #define POOL_DAY_ERROR(...) \
-  log_msg(LOG_SEVERITY_ERROR, LOG_HEADER " | " LOG_TAG_ERROR ": " __VA_ARGS__)
+  log_msg(LOG_HEADER " | " LOG_TAG_ERROR ": " __VA_ARGS__)
 #else
 #define POOL_DAY_LOG(...)
 #define POOL_DAY_ERROR(...)
 #endif  // LIB_LOGGING
 
 /**
- * @brief Log severity definitions.
- */
-typedef enum {
-  LOG_SEVERITY_INFO,
-  LOG_SEVERITY_ERROR
-} log_severity_t;
-
-/**
  * @brief Log a message to the screen.
  *
  * @note This function is thread safe.
  *
- * @param sev Log severity.
  * @param fmt Message format to be logged.
  */
-void log_msg(log_severity_t sev, const char *fmt, ...);
+void log_msg(const char *fmt, ...);
 
 #endif  // LOGGER_H_
--- src/logger.c
+++ src/logger.c
@@ -8,13 +8,12 @@
 
 static pthread_mutex_t log_mutex;  //!< Mutex for log operations.
 
-void __attribute__((unused)) log_msg(log_severity_t sev, const char *fmt, ...) {
+void __attribute__((unused)) log_msg(const char *fmt, ...) {
   va_list args;
-  FILE *out = (sev == LOG_SEVERITY_INFO) ? stdout : stderr;
 
   THREAD_SAFE_ZONE(&log_mutex, {
     va_start(args, fmt);
-    vfprintf(out, fmt, args);
+    vprintf(fmt, args);
     va_end(args);
   });
 }
