24f1f7863a99543fd954c0f62b4ec400825adc5a
andré <andrelcmoreira@disroot.org>
2023-12-22 01:44:43 -0300

    minor refactoring

--- src/logger.c
+++ src/logger.c
@@ -2,16 +2,16 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 #include <pthread.h>
 
 #include "core/utils.h"
 
-#define GET_LOG_FILE(sev) (sev == POOL_DAY_INFO_INFO) ? stdout : stderr
-#define GET_LOG_TAG(sev)  (sev == POOL_DAY_INFO_INFO) ? "INFO" : "ERROR"
+#define GET_LOG_FILE(sev)    (sev == POOL_DAY_INFO_INFO) ? stdout : stderr
+#define GET_LOG_TAG(sev)     (sev == POOL_DAY_INFO_INFO) ? "INFO" : "ERROR"
+#define GET_FILE_NAME(path)  strrchr(path, '/') + 1
 
-#define FILENAME(path)    strrchr(path, '/') + 1
-
-#define LOG_FMT        "%s:%d\t| %s\t| %s: "
+#define LOG_FMT           "%s:%d\t| %s\t| %s: "
 #define LOG_BUFFER_SIZE   4096
 
 static pthread_mutex_t log_mutex;  //!< Mutex for log operations.
@@ -25,7 +25,7 @@ void __log_msg(pool_day_log_severity_t sev, const char *file_name,
     va_start(args, fmt);
 
     vsnprintf(log_buffer, LOG_BUFFER_SIZE, fmt, args);
-    fprintf(GET_LOG_FILE(sev), LOG_FMT "%s\n", FILENAME(file_name), line_no,
+    fprintf(GET_LOG_FILE(sev), LOG_FMT "%s\n", GET_FILE_NAME(file_name), line_no,
             func_name, GET_LOG_TAG(sev), log_buffer);
 
     va_end(args);
