088a6c557c1649dd28077b820c0a4648cb01375c
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-10-19 02:02:14 -0300

    licensing the project under the LGPLv3

--- include/internal/queue.h
+++ include/internal/queue.h
@@ -2,6 +2,23 @@
  * @file
  *
  * @brief Contains the basic definitions and data structures of the task queue.
+ *
+ * @authors
+ * Copyright (C) 2023 André L. C. Moreira <andrelcmoreira@disroot.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef QUEUE_H_
 #define QUEUE_H_
--- include/internal/utils.h
+++ include/internal/utils.h
@@ -2,6 +2,23 @@
  * @file
  *
  * @brief Contains a set of utilities available for use by the library.
+ *
+ * @authors
+ * Copyright (C) 2023 André L. C. Moreira <andrelcmoreira@disroot.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #ifndef UTILS_H_
 #define UTILS_H_
