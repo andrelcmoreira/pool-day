4ef5f74a36496a2739133236d3d09dcf458ba83a
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-10-19 14:07:03 -0300

    python: adding docstrings

--- python/pool_day/pool_day.py
+++ python/pool_day/pool_day.py
@@ -7,6 +7,9 @@ from pool_day.c_defs import _pd_handle, _Task, _PoolDay
 class PoolDay:
 
     """
+    PoolDay instance.
+
+    Implements a wrapper around the pool-day C library.
     """
 
     def __init__(self, pool: _PoolDay):
@@ -14,27 +17,47 @@ class PoolDay:
 
     def enqueue_task(self, task: _Task):
         """
+        Enqueue a task to the pool.
+
+        :task: The task instance.
+        :return: 0 on success, the suitable error code on failure.
         """
         return _pd_handle.enqueue_task(self._pool, task)
 
     def abort_tasks(self):
         """
+        Abort all pending tasks in the pool.
+
+        :return: 0 on success, the suitable error code on failure.
         """
         return _pd_handle.abort_tasks(self._pool)
 
     def queued_tasks(self) -> int:
         """
+        Get the number of queued tasks in the pool.
+
+        :return: Number of queued tasks.
         """
         return _pd_handle.queued_tasks(self._pool)
 
     def wait_task_finish(self, task: _Task):
         """
+        Wait for a task to finish.
+
+        :task: The task instance.
+        :return: The return value of the task's callback function.
         """
         return _pd_handle.wait_task_finish(self._pool, task)
 
 
 def create_task(cb, param) -> _Task:
     """
+    Create a new pool's task instance.
+
+    :cb: The callback function.
+    :param: The parameter passed to the callback function.
+
+    :return: A new task instance.
     """
     return _pd_handle.create_task(cb, param)
 
@@ -42,6 +65,11 @@ def create_task(cb, param) -> _Task:
 @contextmanager
 def create_pool(size: int):
     """
+    Create a new pool instance.
+
+    :size: Number of threads in the pool.
+
+    :return: A pool-day instance.
     """
     pool = _pd_handle.create_pool(size)
     yield PoolDay(pool)
