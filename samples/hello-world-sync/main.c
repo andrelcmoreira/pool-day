24bf43f0996e80472a886a8fd5f2a4db38dab03e
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-12-06 23:09:43 -0300

    samples: splitting 'hello-world' sample into 'hello-world-sync' and 'hello-world-async'

---
+++ samples/hello-world-sync/main.c
@@ -0,0 +1,53 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "pool_day.h"
+
+#define HELLO_WORLD_TASK_ID 0
+
+void *func(void *param) {
+  char *str = (char *)param;
+  char *ret = (char *)calloc(10, sizeof(char));
+
+  for (int i = 0; i < 10; i++) {
+    printf("%s %d\n", str, i);
+    usleep(1000000);
+  }
+
+  strcat(ret, "success!");
+
+  return ret;
+}
+
+int main(void) {
+  pool_day_t pool;
+
+  pool = create_pool(1);
+  if (!pool) {
+    // handle error
+    exit(EXIT_FAILURE);
+  }
+
+  char str[] = "hello, world!";
+  task_t *task = create_task(HELLO_WORLD_TASK_ID, func, (void *)str,
+                             sizeof(char) * strlen(str) + 1, NULL, NULL);
+
+  if (!task) {
+    // handle error
+    exit(EXIT_FAILURE);
+  }
+
+  assert(enqueue_task(pool, task) == POOL_DAY_SUCCESS);
+  char *ret = (char *)get_task_result(task);
+
+  printf("result = %s\n", ret);
+
+  destroy_task(task);
+  destroy_pool(&pool);
+  free(ret);
+
+  exit(EXIT_SUCCESS);
+}
