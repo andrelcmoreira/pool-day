9eb789fb82be329aeb8cfb762ea084556cff0fe6
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-12-23 23:29:04 -0300

    lib: fixing cppcheck and doxygen issues

--- src/task.c
+++ src/task.c
@@ -5,12 +5,12 @@
 
 #include "internal/task_def.h"
 
+// cppcheck-suppress unusedFunction
 task_t create_sync_task(uint32_t id, void *(*task)(void *), void *param,
                         size_t param_size) {
   return create_async_task(id, task, param, param_size, false, NULL, NULL);
 }
 
-// cppcheck-suppress unusedFunction
 task_t create_async_task(uint32_t id, void *(*task)(void *), void *param,
                          size_t param_size, bool auto_release,
                          void (*start_cb)(uint32_t, void *),
