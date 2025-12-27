39bf4f088d10d64354802988f432e30f031059fc
André L. C. Moreira <andrelcmoreira@proton.me>
2025-12-27 19:53:24 -0300

    samples: fixing compile errors

--- samples/hello-world-sync/main.c
+++ samples/hello-world-sync/main.c
@@ -33,8 +33,8 @@ int main(void) {
   }
 
   char str[] = "hello, world!";
-  task_t task = create_task(HELLO_WORLD_TASK_ID, func, (void *)str,
-                            sizeof(char) * strlen(str) + 1, NULL, NULL);
+  task_t task = create_sync_task(HELLO_WORLD_TASK_ID, func, (void *)str,
+                                 sizeof(char) * strlen(str) + 1);
 
   if (!task) {
     // handle error
