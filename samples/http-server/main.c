a2df3d51e57e693808c8ad643836d2561eea18fd
André L. C. Moreira <andrelcmoreira@proton.me>
2026-04-03 19:31:44 -0300

    samples: adding the content-type verification to the http-server sample

--- samples/http-server/main.c
+++ samples/http-server/main.c
@@ -22,9 +22,16 @@
 #define DEFAULT_PORT           8080
 #define DEFAULT_MAX_CLIENTS    100
 
-#define STR(x) #x
-#define MAKE_ERROR_BODY(status_code, msg) \
-  "<html><body><h1>" STR(status_code) " " #msg "</h1></body></html>"
+#define CONTENT_TYPE_TEXT_HTML  "text/html"
+#define CONTENT_TYPE_IMAGE_PNG  "image/png"
+#define CONTENT_TYPE_IMAGE_JPEG "image/jpeg"
+#define CONTENT_TYPE_IMAGE_GIF  "image/gif"
+#define CONTENT_TYPE_TEXT_CSS   "text/css"
+#define CONTENT_TYPE_APP_JS     "application/javascript"
+#define CONTENT_TYPE_APP_OCTET  "application/octet-stream"
+
+#define MAKE_ERROR_BODY(code, msg) \
+  "<html><body><h1>" #code " " #msg "</h1></body></html>"
 
 typedef struct {
   uint32_t max_clients;
@@ -41,27 +48,6 @@ typedef struct {
   uint32_t fd;
   struct in_addr addr;
 } client_t;
-
-typedef enum {
-  SUCCESS,
-  NOT_FOUND,
-  INTERNAL_ERROR
-} http_status_id_t;
-
-typedef struct {
-  http_status_id_t id;
-  int code;
-  char *msg;
-} http_status_t;
-
-//static http_status_t status_table[] = {
-//  { SUCCESS, 200, "OK" },
-//  { NOT_FOUND, 404, "Not Found" },
-//  { INTERNAL_ERROR, 505, "Internal Error" }
-//};
-
-// TODO: lookup table for http errors
-// TODO: 501 error?
 
 static void sig_handler(int signum) {
   (void)signum;
@@ -87,168 +73,216 @@ static void on_client_disconnected(uint32_t tid, const void *param,
   }
 }
 
+static const char *get_content_type(const char *path) {
+  const char *ext = strrchr(path, '.');
+
+  if (!ext) {
+    return CONTENT_TYPE_APP_OCTET;
+  }
+
+  if (!strcmp(ext, ".html") || !strcmp(ext, ".htm")) {
+    return CONTENT_TYPE_TEXT_HTML;
+  }
+
+  if (!strcmp(ext, ".png")) {
+    return CONTENT_TYPE_IMAGE_PNG;
+  }
+
+  if (!strcmp(ext, ".jpg") || !strcmp(ext, ".jpeg")) {
+    return CONTENT_TYPE_IMAGE_JPEG;
+  }
+
+  if (!strcmp(ext, ".gif")) {
+    return CONTENT_TYPE_IMAGE_GIF;
+  }
+
+  if (!strcmp(ext, ".css")) {
+    return CONTENT_TYPE_TEXT_CSS;
+  }
+
+  if (!strcmp(ext, ".js")) {
+    return CONTENT_TYPE_APP_JS;
+  }
+
+  return CONTENT_TYPE_APP_OCTET;
+}
+
 static void parse_request(const char *buffer, request_t *req) {
-  // simple parsing logic for demonstration purposes
   sscanf(buffer, "%s %s", req->method, req->resource);
 }
 
 static char *build_http_header(int status_code, const char *status_str,
+                               const char *content_type, size_t content_length,
                                size_t *header_len) {
-  const char *crlf = "\r\n";
-  const char *version = "HTTP/1.1";
-  const char *content_type = "Content-Type: text/html"; // TODO: and how about images?
-  const char *header_fmt =
-    "%s %d %s\r\n"
-    "%s\r\n"
+
+  const char *fmt =
+    "HTTP/1.1 %d %s\r\n"
+    "Content-Type: %s\r\n"
+    "Content-Length: %zu\r\n"
+    "Connection: close\r\n"
     "\r\n";
 
-  //const char *reply_fmt =
-  //  "HTTP/1.1 %d %s\r\n"
-  //  "Content-Type: text/html\r\n"
-  //  "\r\n%s";
+  *header_len = snprintf(NULL, 0, fmt, status_code, status_str, content_type,
+                         content_length);
 
-  *header_len = strlen(version) + sizeof(int) + strlen(status_str) + 2
-    + 3 * strlen(crlf) + strlen(content_type);
-
-  char *header = calloc(1, *header_len);
+  char *header = malloc(*header_len + 1);
   if (!header) {
     return NULL;
   }
 
-  snprintf(header, *header_len, header_fmt, version, status_code, status_str,
-           content_type);
+  snprintf(header, *header_len + 1, fmt, status_code, status_str, content_type,
+           content_length);
 
   return header;
 }
 
-static int assemble_reply(char **buffer, char *header, size_t header_len,
+static int assemble_reply(char **buffer, const char *header, size_t header_len,
                           const char *body, size_t body_len) {
-  size_t buffer_len = header_len + body_len + 1;
+  size_t total = header_len + body_len;
 
-  *buffer = calloc(1, buffer_len);
+  *buffer = malloc(total);
   if (!(*buffer)) {
     return 0;
   }
 
   memcpy(*buffer, header, header_len);
-  memcpy(*buffer + header_len - 1, body, body_len);
+  memcpy(*buffer + header_len, body, body_len);
 
-  return buffer_len;
+  return total;
 }
 
-static char *get_resource(const char *res_name, size_t *resource_len) {
+static char *get_resource(const char *path, size_t *len) {
   struct stat st;
 
-  FILE *file = fopen(res_name, "r"); // TODO: and how about the image files?
+  FILE *file = fopen(path, "rb");
   if (!file) {
-    fprintf(stderr, "[-] fail to open the requested resource: %s\n",
-            strerror(errno));
     return NULL;
   }
 
-  stat(res_name, &st);
-
-  char *content = calloc(1, st.st_size + 1);
-  if (!content) {
-    fprintf(stderr, "[-] fail to allocate memory for the requested resource\n");
+  if (stat(path, &st) < 0) {
     fclose(file);
     return NULL;
   }
 
-  *resource_len = fread(content, 1, st.st_size, file);
-  if (!(*resource_len)) {
-    fprintf(stderr, "[-] fail to read the requested resource\n");
-    free(content);
+  char *data = malloc(st.st_size);
+  if (!data) {
     fclose(file);
     return NULL;
   }
 
+  size_t read_bytes = fread(data, 1, st.st_size, file);
   fclose(file);
 
-  return content;
+  if (read_bytes != (size_t)st.st_size) {
+    free(data);
+    return NULL;
+  }
+
+  *len = read_bytes;
+  return data;
 }
 
 static int handle_get_request(char **reply_buffer, size_t *reply_buffer_size,
                               const char *resource) {
-  int status_code;
-  size_t res_len;
-  char *header;
   size_t header_len;
+
+  if (strstr(resource, "..")) {
+    return 400;
+  }
+
+  size_t res_len;
   char *res = get_resource(resource, &res_len);
 
   if (res) {
-    status_code = 200;
-    header = build_http_header(status_code, "OK", &header_len);
+    const char *content_type = get_content_type(resource);
+    char *header = build_http_header(200, "OK", content_type, res_len,
+                                     &header_len);
+
     *reply_buffer_size = assemble_reply(reply_buffer, header, header_len, res,
                                         res_len);
+
+    free(header);
     free(res);
-  } else {
-    status_code = 404;
-    header = build_http_header(status_code, "Not Found", &header_len);
-    char *body = MAKE_ERROR_BODY(404, Not Found);
-    *reply_buffer_size = assemble_reply(reply_buffer, header, header_len,
-                                        body, strlen(body));
+
+    return 200;
   }
+
+  const char *body = MAKE_ERROR_BODY(404, Not Found);
+  size_t body_len = strlen(body);
+  char *header = build_http_header(404, "Not Found", CONTENT_TYPE_TEXT_HTML,
+                                   body_len, &header_len);
+
+  *reply_buffer_size = assemble_reply(reply_buffer, header, header_len, body,
+                                      body_len);
 
   free(header);
 
-  return status_code;
+  return 404;
 }
 
-static int handle_request(const char *req_buffer, char **reply_buffer,
+static int handle_request(const char *req_buffer,
+                          char **reply_buffer,
                           size_t *reply_buffer_len) {
+
   request_t req;
 
   memset(&req, 0, sizeof(request_t));
 
   parse_request(req_buffer, &req);
-
-  fprintf(stdout, "[+] received request: %s %s\n", req.method, req.resource);
-  if (strcmp(req.method, "GET")) {
-    fprintf(stderr, "[-] unsupported request\n");
-    // TODO: assemble 501 reply
+  if (strcmp(req.method, "GET") != 0) {
     return 501;
   }
 
-  return handle_get_request(reply_buffer, reply_buffer_len, &req.resource[1]);
+  fprintf(stdout, "[+] %s %s\n", req.method, req.resource);
+
+  const char *resource = req.resource[0] == '/' ?
+                         req.resource + 1 :
+                         req.resource;
+
+  return handle_get_request(reply_buffer, reply_buffer_len, resource);
+}
+
+static void send_all(int fd, const char *buffer, size_t len) {
+  size_t total = 0;
+
+  while (total < len) {
+    ssize_t sent = send(fd, buffer + total, len - total, 0);
+
+    if (sent <= 0) {
+      break;
+    }
+
+    total += sent;
+  }
 }
 
 static void *handle_new_connection(void *param) {
   client_t *cli = (client_t *)param;
   int *ret = NULL;
-  char *req_buffer;
-  char *reply_buffer;
-  size_t reply_buffer_len;
 
-  req_buffer = calloc(1, MAX_BUFFER_SIZE);
+  char *req_buffer = calloc(1, MAX_BUFFER_SIZE);
   if (!req_buffer) {
-    fprintf(stderr, "[-] fail to allocate memory for request buffer\n");
     close(cli->fd);
     return NULL;
   }
 
   ssize_t received = recv(cli->fd, req_buffer, MAX_BUFFER_SIZE - 1, 0);
+
   if (received > 0) {
+    char *reply_buffer = NULL;
+    size_t reply_len = 0;
+
     ret = calloc(1, sizeof(int));
-    if (ret) {
-      *ret = handle_request(req_buffer, &reply_buffer, &reply_buffer_len);
+    *ret = handle_request(req_buffer, &reply_buffer, &reply_len);
 
-      printf("reply_len = %lu\n", reply_buffer_len);
-      printf("reply = %s\n", reply_buffer);
-
-      //for (size_t i = 0; i < reply_buffer_len; i++) {
-      //  printf("%c\n", reply_buffer[i]);
-      //}
-
-      if (reply_buffer_len) {
-        send(cli->fd, reply_buffer, reply_buffer_len - 2, 0); // FIXME: tem coisa amais sendo copiado: GDB!!!!!
-        free(reply_buffer);
-      }
+    if (reply_buffer && reply_len > 0) {
+      send_all(cli->fd, reply_buffer, reply_len);
+      free(reply_buffer);
     }
   }
 
-  close(cli->fd);
   free(req_buffer);
+  close(cli->fd);
 
   return (void *)ret;
 }
@@ -259,13 +293,13 @@ static void fill_cfg(int argc, char **argv, server_cfg_t *cfg) {
   while ((opt = getopt(argc, argv, "m:p:r:")) != -1) {
     switch (opt) {
     case 'm':
-      cfg->max_clients = (uint32_t)atoi(optarg);
+      cfg->max_clients = atoi(optarg);
       break;
     case 'p':
-      cfg->port = (uint16_t)atoi(optarg);
+      cfg->port = atoi(optarg);
       break;
     case 'r':
-      memcpy(cfg->root_dir, optarg, strlen(optarg) + 1);
+      strcpy(cfg->root_dir, optarg);
       break;
     }
   }
@@ -278,8 +312,8 @@ static void fill_cfg(int argc, char **argv, server_cfg_t *cfg) {
     cfg->port = DEFAULT_PORT;
   }
 
-  if (cfg->root_dir[0] == '\0') {
-    memcpy(cfg->root_dir, DEFAULT_ROOT_DIR, strlen(DEFAULT_ROOT_DIR) + 1);
+  if (!cfg->root_dir[0]) {
+    strcpy(cfg->root_dir, DEFAULT_ROOT_DIR);
   }
 }
 
@@ -310,17 +344,14 @@ static int setup_socket(int *sock_fd, const server_cfg_t *cfg) {
   return 0;
 }
 
-static int setup_server(const server_cfg_t *cfg, int *server_fd,
+static int setup_server(int *sock_fd, const server_cfg_t *cfg,
                         pool_day_t *pool) {
-  if (setup_socket(server_fd, cfg) != 0) {
-    fprintf(stderr, "[-] fail to setup the server socket: %s\n",
-            strerror(errno));
+  if (setup_socket(sock_fd, cfg) != 0) {
     return 1;
   }
 
   if (!(*pool = create_pool(cfg->max_clients))) {
-    fprintf(stderr, "[-] fail to setup the server pool\n");
-    close(*server_fd);
+    close(*sock_fd);
     return 1;
   }
 
@@ -328,27 +359,22 @@ static int setup_server(const server_cfg_t *cfg, int *server_fd,
 }
 
 static int server_mainloop(int server_fd, pool_day_t pool) {
-  int ret;
   fd_set set;
-  struct sockaddr_in cli_addr;
-  socklen_t cli_len = sizeof(cli_addr);
 
   while (1) {
     FD_ZERO(&set);
     FD_SET(server_fd, &set);
 
-    ret = select(server_fd + 1, &set, NULL, NULL, NULL);
-    if ((ret == -1) && (errno == EINTR)) {
-      fprintf(stdout, "[+] exiting server...\n");
+    if (select(server_fd + 1, &set, NULL, NULL, NULL) < 0) {
       break;
     }
 
     if (FD_ISSET(server_fd, &set)) {
-      int client_fd = accept(server_fd, (struct sockaddr *)&cli_addr, &cli_len);
+      struct sockaddr_in cli_addr;
+      socklen_t len = sizeof(cli_addr);
 
-      if (client_fd == -1) {
-        fprintf(stderr, "[-] fail to accept the incoming client: %s\n",
-                strerror(errno));
+      int client_fd = accept(server_fd, (struct sockaddr *)&cli_addr, &len);
+      if (client_fd < 0) {
         continue;
       }
 
@@ -360,32 +386,29 @@ static int server_mainloop(int server_fd, pool_day_t pool) {
                                       on_client_connected,
                                       on_client_disconnected);
 
-      if (enqueue_task(pool, task) != POOL_DAY_SUCCESS) {
-        fprintf(stderr, "[-] fail to enqueue the request task\n");
-        destroy_task(task);
-        close(client_fd);
-      }
+      enqueue_task(pool, task);
     }
   }
 
   close(server_fd);
   destroy_pool(&pool);
 
-  return EXIT_SUCCESS;
+  return 0;
 }
 
 static int run_server(const server_cfg_t *cfg) {
   int server_fd;
   pool_day_t pool;
 
-  if (setup_server(cfg, &server_fd, &pool)) {
+  if (setup_server(&server_fd, cfg, &pool)) {
     fprintf(stderr, "[-] fail to setup the server\n");
-    return EXIT_FAILURE;
+    return 1;
   }
 
   if (chdir(cfg->root_dir)) {
-    fprintf(stderr, "[-] fail to run the server: %s\n", strerror(errno));
-    return EXIT_FAILURE;
+    fprintf(stderr, "[-] fail to run the server on '%s': %s\n", cfg->root_dir,
+            strerror(errno));
+    return 1;
   }
 
   fprintf(stdout,
@@ -398,13 +421,10 @@ static int run_server(const server_cfg_t *cfg) {
 int main(int argc, char **argv) {
   server_cfg_t cfg;
 
-  memset(&cfg, 0, sizeof(server_cfg_t));
-
   signal(SIGINT, sig_handler);
-  signal(SIGTERM, sig_handler);
 
+  memset(&cfg, 0, sizeof(server_cfg_t));
   fill_cfg(argc, argv, &cfg);
-  int ret = run_server(&cfg);
 
-  exit(ret);
+  return run_server(&cfg);
 }
